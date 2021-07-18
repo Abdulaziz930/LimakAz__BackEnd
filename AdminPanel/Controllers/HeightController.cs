1a4a389b54961c20b93ee047e8d754e50e9b3f44
Abdulaziz930 <abdulazizak@code.edu.az>
2021-07-18 21:01:49 +0400

    height and box count input's CRUD is done

---
+++ AdminPanel/Controllers/HeightController.cs
@@ -0,0 +1,189 @@
+﻿using DataAccess.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Controllers
+{
+    public class HeightController : Controller
+    {
+        private readonly IRepository<HeightInput> _repository;
+        private readonly IRepository<Language> _languageRepository;
+
+        public HeightController(IRepository<HeightInput> repository, IRepository<Language> languageRepository)
+        {
+            _repository = repository;
+            _languageRepository = languageRepository;
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            var allHeightInputs = await _repository.GetAllAsync(x => x.IsDeleted == false, null);
+            ViewBag.PageCount = Decimal.Ceiling((decimal)allHeightInputs.Count / 5);
+            ViewBag.Page = page;
+
+            if (ViewBag.PageCount < page || page <= 0)
+                return NotFound();
+
+            var heightInputs = await _repository.GetAll(x => x.IsDeleted == false, null)
+                .OrderByDescending(x => x.Id).Skip((page - 1) * 5).Take(5).ToListAsync();
+            if (heightInputs == null)
+                return NotFound();
+
+            return View(heightInputs);
+        }
+
+        #region Create
+
+        public async Task<IActionResult> Create()
+        {
+            var languages = await _languageRepository.GetAllAsync(x => x.IsDeleted == false, null);
+            ViewBag.Languages = languages;
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HeightInput heightInput, int? languageId)
+        {
+            var languages = await _languageRepository.GetAllAsync(x => x.IsDeleted == false, null);
+            ViewBag.Languages = languages;
+
+            if (!ModelState.IsValid)
+            {
+                return View(heightInput);
+            }
+
+            if (languageId == null)
+            {
+                ModelState.AddModelError("", "Please select language.");
+                return View();
+            }
+
+            if (languages.All(x => x.Id != languageId.Value))
+                return BadRequest();
+
+            heightInput.LanguageId = languageId.Value;
+
+            await _repository.CreateAsync(heightInput);
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Update
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var languages = await _languageRepository.GetAllAsync(x => x.IsDeleted == false, null);
+            ViewBag.Languages = languages;
+
+            var heightInput = await _repository.GetAsync(x => x.Id == id && x.IsDeleted == false, null);
+            if (heightInput == null)
+                return NotFound();
+
+            return View(heightInput);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, HeightInput heightInput, int? languageId)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var languages = await _languageRepository.GetAllAsync(x => x.IsDeleted == false, null);
+            ViewBag.Languages = languages;
+
+            if (!ModelState.IsValid)
+            {
+                return View(heightInput);
+            }
+
+            if (languageId == null)
+            {
+                ModelState.AddModelError("", "Please select language.");
+                return View();
+            }
+
+            if (languages.All(x => x.Id != languageId.Value))
+                return BadRequest();
+
+            await _repository.UpdateAsync(heightInput);
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var includedProperties = new List<string>
+            {
+                nameof(Language)
+            };
+
+            var heightInput = await _repository.GetAsync(x => x.Id == id && x.IsDeleted == false, includedProperties);
+            if (heightInput == null)
+                return NotFound();
+
+            return View(heightInput);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteSection(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var heightInput = await _repository.GetAsync(x => x.Id == id && x.IsDeleted == false, null);
+            if (heightInput == null)
+                return NotFound();
+
+            heightInput.IsDeleted = true;
+
+            await _repository.UpdateAsync(heightInput);
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Detail
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var includedProperties = new List<string>
+            {
+                nameof(Language)
+            };
+
+            var heightInput = await _repository.GetAsync(x => x.Id == id && x.IsDeleted == false, includedProperties);
+            if (heightInput == null)
+                return NotFound();
+
+            return View(heightInput);
+        }
+
+        #endregion
+    }
+}
