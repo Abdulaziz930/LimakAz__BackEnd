4f5827ac3171b71d91252bad60845ca675f51411
Abdulaziz930 <abdulazizak@code.edu.az>
2021-09-28 17:45:24 +0400

    get services with reflection

---
+++ Commonn/Extensions.cs
@@ -0,0 +1,52 @@
+﻿using Buisness.Abstract;
+using DataAccess;
+using DataAccess.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// This is extension method for service collections,
+        /// the purpose of the method is to register all types
+        /// </summary>
+        /// <param name="param">An object representing a type in the assembly that will be returned.</param>
+        /// <returns>The assembly in which the specified type is defined.</returns>
+        public static IServiceCollection RegisterAllTypes(this IServiceCollection services, ServiceLifetime serviceLifetime, Type param)
+        {
+            var assembly = Assembly.GetAssembly(param);
+
+            var types = assembly.GetTypes().Where(x => x.IsInterface && x.IsAbstract)
+                .Select(t => new
+                {
+                    Service = t,
+                    Implementation = assembly.GetTypes().FirstOrDefault(x => t.IsAssignableFrom(x))
+                }).ToList();
+
+            foreach (var type in types)
+            {
+                if (serviceLifetime == ServiceLifetime.Scoped)
+                {
+                    services.AddScoped(type.Service, type.Implementation);
+                }
+                else if(serviceLifetime == ServiceLifetime.Singleton)
+                {
+                    services.AddSingleton(type.Service, type.Implementation);
+                }
+                else
+                {
+                    services.AddTransient(type.Service, type.Implementation);
+                }
+            }
+
+            return services;
+        }
+    }
+}
--- Utils/Extensions.cs
+++ Utils/Extensions.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Web;
 
 namespace Utils
 {
